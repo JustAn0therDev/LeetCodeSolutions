74dc351a95f9dc1f3f38dfc5af76da5084afcfb9
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:14:04 -0300

    Added solution to problem "Maximum Depth of Binary Tree"

---
+++ Easy/MaxDepth.cs
@@ -0,0 +1,28 @@
+﻿using AuxiliaryClasses;
+
+namespace Easy;
+
+public class MaxDepth
+{
+    public int Solve(TreeNode? root)
+    {
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftDepth = 0;
+        int rightDepth = 0;
+
+        if (root.left != null)
+        {
+            leftDepth += Solve(root.left);
+        }
+
+        if (root.right != null)
+        {
+            rightDepth += Solve(root.right);
+        }
+
+        return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+    }
+}
