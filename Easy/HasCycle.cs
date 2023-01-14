e14677b9160e9c7a2d548d4a27c4aa14334fe0a8
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:24:33 -0300

    Added solution to problem "Linked List Cycle"

---
+++ Easy/HasCycle.cs
@@ -0,0 +1,27 @@
+﻿using AuxiliaryClasses;
+
+namespace Easy;
+
+public class HasCycle
+{
+    public bool Solve(ListNode? head)
+    {
+        bool result = false;
+        List<ListNode> dp = new();
+
+        while (head != null)
+        {
+            // It uses Object.Equals for object comparison, meaning it compares pointers and not values.
+            if (dp.Contains(head))
+            {
+                result = true;
+                break;
+            }
+
+            dp.Add(head);
+            head = head.next;
+        }
+
+        return result;
+    }
+}
