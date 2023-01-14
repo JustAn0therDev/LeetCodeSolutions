b77b9996377a5b544cb02d20c555abd05d9f9786
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 15:59:00 -0300

    Added solution to problem "Merge Sorted Array"

---
+++ Easy/MergeSortedArray.cs
@@ -0,0 +1,19 @@
+﻿namespace Easy;
+
+public class MergeSortedArray
+{
+    public void Solve(int[] nums1, int m, int[] nums2, int n)
+    {
+        List<int> ans = new();
+
+        ans.AddRange(nums1.Take(nums1.Length - n));
+        ans.AddRange(nums2);
+
+        ans.Sort();
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            nums1[i] = ans[i];
+        }
+    }
+}
