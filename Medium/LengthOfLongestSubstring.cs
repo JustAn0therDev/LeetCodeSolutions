e912ef6bb46baa63b5ebef73154c7ff9fd5a7d03
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:22:39 -0300

    Renamed Class1.cs to AddTwoNumbers.cs and added solution to problem "Longest Substring Without Repeating Characters"

---
+++ Medium/LengthOfLongestSubstring.cs
@@ -0,0 +1,34 @@
+﻿namespace Medium;
+
+public class LengthOfLongestSubstring
+{
+    public int Solve(string s)
+    {
+        List<char> dp = new();
+        int longest = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            dp.Add(s[i]);
+
+            for (int j = i + 1; j < s.Length; j++)
+            {
+                if (dp.Contains(s[j]))
+                {
+                    break;
+                }
+
+                dp.Add(s[j]);
+            }
+
+            if (longest < dp.Count)
+            {
+                longest = dp.Count;
+            }
+
+            dp.Clear();
+        }
+
+        return longest;
+    }
+}
