5ca21736132e83c6235098f0e4db6f720a730914
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 15:57:21 -0300

    Added solution to problem "Convert To Temperature"

---
+++ Easy/ConvertTheTemperature.cs
@@ -0,0 +1,9 @@
+﻿namespace Easy;
+
+public class ConvertTheTemperature
+{
+    public double[] Solve(double celsius)
+    {
+        return new [] { celsius + 273.15, celsius * 1.80 + 32 };
+    }
+}
