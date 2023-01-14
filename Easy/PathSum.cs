e627f5ca7a591b7cb47fde90c923e22791ce33ea
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:17:49 -0300

    Added solution to problem "Path Sum"

---
+++ Easy/PathSum.cs
@@ -0,0 +1,34 @@
+﻿using AuxiliaryClasses;
+
+namespace Easy;
+
+public class PathSum
+{
+    public bool HasPathSum(TreeNode root, int targetSum, int currentSum = 0)
+    {
+        bool result = false;
+
+        if (root == null)
+        {
+            return false;
+        }
+
+        // it's a leaf node
+        if (root.left == null && root.right == null)
+        {
+            return targetSum == currentSum + root.val;
+        }
+
+        if (root.left != null)
+        {
+            result = HasPathSum(root.left, targetSum, currentSum + root.val);
+        }
+
+        if (!result && root.right != null)
+        {
+            result = HasPathSum(root.right, targetSum, currentSum + root.val);
+        }
+
+        return result;
+    }
+}
