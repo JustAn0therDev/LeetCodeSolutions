99f0ad1b1e6077917b623974b81d9e99b5903cee
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 15:54:55 -0300

    Initial commit

---
+++ Easy/SubtractProductAndSum.cs
@@ -0,0 +1,18 @@
+﻿namespace Easy;
+
+public class SubtractProductAndSum
+{
+    public int Solve(int n) {
+        int multiply = 1;
+        int sum = 0;
+        string digits = n.ToString();
+
+        foreach (char digit in digits)
+        {
+            multiply *= (int)char.GetNumericValue(digit);
+            sum += (int)char.GetNumericValue(digit);
+        }
+        
+        return multiply - sum;
+    }
+}
