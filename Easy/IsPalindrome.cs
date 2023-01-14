ce27a9fcbd21115f6dc95e6e8e3debb0b1c53a85
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:08:18 -0300

    Added solution to problem "Is Palindrome"

---
+++ Easy/IsPalindrome.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Easy;
+
+public class IsPalindrome
+{
+    public bool Solve(string s)
+    {
+        string treatedString = Regex.Replace(s, "[^A-Za-z0-9]", "").ToLower();
+        return treatedString == string.Join("", treatedString.Reverse());
+    }
+}
