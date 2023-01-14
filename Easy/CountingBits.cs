923dc46e8cda99b6a3103bbd2d3e118f5da98671
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-14 16:16:28 -0300

    Added solution to problem "Counting Bits"

---
+++ Easy/CountingBits.cs
@@ -0,0 +1,13 @@
+﻿namespace Easy;
+
+public class CountingBits
+{
+    public int[] Solve(int n)
+    {
+        return Enumerable
+            .Range(0, n + 1)
+            .Select(
+                s => Convert.ToString(s, 2).Count(c => c == '1')
+            ).ToArray();
+    }
+}
