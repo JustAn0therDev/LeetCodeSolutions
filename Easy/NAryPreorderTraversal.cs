2b396dc069c32000c702c506426379975ee02693
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-19 17:33:04 -0300

    Added solution to problem "N-ary Tree Preorder Traversal"

---
+++ Easy/NAryPreorderTraversal.cs
@@ -0,0 +1,25 @@
+﻿using AuxiliaryClasses;
+
+namespace Easy;
+
+public class NAryPreorderTraversal
+{
+    public IList<int> Solve(Node? root, List<int>? l = null)
+    {
+        l ??= new();
+
+        if (root == null)
+        {
+            return l;
+        }
+        
+        l.Add(root.val);
+
+        foreach (Node child in root.children)
+        {
+            Solve(child, l);
+        }
+
+        return l;
+    }
+}
--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -1,12 +1,2 @@
 ﻿using Medium;
 
-char[][] board = { new[] {'A','B','C','E'}, new[] {'S','F','C','S'}, new[] {'A','D','E','E'} };
-string word = "ABCCED";
-
-Console.WriteLine(SearchWord.Solve(board, word));
-
-word = "SEE";
-Console.WriteLine(SearchWord.Solve(board, word));
-
-word = "ABCB";
-Console.WriteLine(SearchWord.Solve(board, word));
