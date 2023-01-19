684adb41544474fd4a21485b5616fcd971bb4070
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-19 19:11:53 -0300

    Added solution to problem "Reverse Linked List"

---
+++ Easy/ReverseLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using AuxiliaryClasses;
+
+namespace Easy;
+
+public class ReverseLinkedList
+{
+    public static ListNode? Solve(ListNode? head)
+    {
+        // First, we begin by traversing the linked list
+        // using a list<listNode> to get the references set in order.
+        List<ListNode> dp = new();
+        while (head != null)
+        {
+            dp.Add(head);
+            head = head.next;
+        }
+
+        // Then, traverse back the linked list from the end to the beginning
+        ListNode? newHead = dp.LastOrDefault();
+
+        if (newHead == null)
+        {
+            return null;
+        }
+
+        ListNode? headPtr = newHead;
+
+        for (int i = dp.Count - 1; i >= 0; i--)
+        {
+            if (i > 0)
+            {
+                // And change "next" pointers and return the reference to the first node.
+                newHead.next = dp[i - 1];
+                newHead = dp[i - 1];
+            }
+            else
+            {
+                newHead.next = null;
+            }
+        }
+
+        return headPtr;
+    }
+}
--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -1,2 +1,12 @@
-﻿using Medium;
+﻿using AuxiliaryClasses;
+using Easy;
 
+ListNode? list = null;
+
+list = ReverseLinkedList.Solve(list);
+
+while (list != null)
+{
+    Console.WriteLine(list.val);
+    list = list.next;
+}
