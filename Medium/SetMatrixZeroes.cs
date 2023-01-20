762f0fa5ff2d966ab295a39029a9ca1f9e76451a
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-20 17:05:51 -0300

    Added solution to problem "Set Matrix Zeroes"

---
+++ Medium/SetMatrixZeroes.cs
@@ -0,0 +1,49 @@
+﻿namespace Medium;
+
+public class SetMatrixZeroes
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        List<(int, int)> changed = new();
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                if (matrix[i][j] != 0 || changed.Contains((i, j))) 
+                    continue;
+
+                int start = 0;
+
+                while (start < matrix[i].Length)
+                {
+                    if (matrix[i][start] == 0 && !changed.Contains((i, start)))
+                    {
+                        start++;
+                        continue;
+                    }
+                    matrix[i][start] = 0;
+                    changed.Add((i, start));
+                    start++;
+                }
+
+                start = 0;
+
+                while (start < matrix.Length)
+                {
+                    if (matrix[start][j] == 0 && !changed.Contains((start, j)))
+                    {
+                        start++;
+                        continue;
+                    }
+                    
+                    matrix[start][j] = 0;
+                    changed.Add((start, j));
+                    start++;
+                }
+                
+                changed.Add((i, j));
+            }
+        }
+    }
+}
