41a89d4323884b6919c304905cab29261fd02282
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-20 17:06:54 -0300

    Added solution to problem "Rotate Image"

---
+++ Medium/RotateImage.cs
@@ -0,0 +1,29 @@
+﻿namespace Medium;
+
+public class RotateImage
+{
+    public void Rotate(int[][] matrix)
+    {
+        List<List<int>> lines = new();
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            List<int> line = new();
+
+            for (int j = matrix.Length - 1; j >= 0; j--)
+            {
+                line.Add(matrix[j][i]);
+            }
+
+            lines.Add(line);
+        }
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                matrix[i][j] = lines[i][j];
+            }
+        }
+    }
+}
