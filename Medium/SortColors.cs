b3cdf8898d3e129536cdcbe448ffb25917b28a92
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-20 17:04:54 -0300

    Added solution to problem "Sort Colors"

---
+++ Medium/SortColors.cs
@@ -0,0 +1,23 @@
+﻿namespace Medium;
+
+public class SortColors
+{
+    public void Solve(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    continue;
+                }
+
+                if (nums[i] < nums[j])
+                {
+                    (nums[j], nums[i]) = (nums[i], nums[j]);
+                }
+            }
+        }
+    }
+}
