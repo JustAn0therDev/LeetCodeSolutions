2db9b5ee5f8d0908a81d89c868edd21244997850
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-25 11:12:28 -0300

    Added solution to problem "Design Parking System"

---
+++ Easy/ParkingSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Easy;
+
+public class ParkingSystem {
+
+    enum ParkingSpotType
+    {
+        Big = 1, Medium = 2, Small = 3
+    }
+    
+    private int Big { get; set; }
+    private int Medium { get; set; }
+    private int Small { get; set; }
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        Big = big;
+        Medium = medium;
+        Small = small;
+    }
+    
+    public bool AddCar(int carType)
+    {
+        ParkingSpotType requestSpotType = (ParkingSpotType)carType;
+
+        if (requestSpotType == ParkingSpotType.Small && Small > 0)
+        {
+            Small--;
+            return true;
+        }
+
+        if (requestSpotType == ParkingSpotType.Medium && Medium > 0)
+        {
+            Medium--;
+            return true;
+        }
+
+        if (requestSpotType == ParkingSpotType.Big && Big > 0)
+        {
+            Big--;
+            return true;
+        }
+
+        return false;
+    }
+}
--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -2,4 +2,9 @@
 
 int[] nums = {1,15,6,3};
 
-Console.WriteLine(DifferenceOfSum.Solve(nums));
+ParkingSystem ps = new(1, 1, 0);
+
+Console.WriteLine(ps.AddCar(1));
+Console.WriteLine(ps.AddCar(2));
+Console.WriteLine(ps.AddCar(3));
+Console.WriteLine(ps.AddCar(1));
