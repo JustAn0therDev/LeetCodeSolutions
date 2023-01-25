51174b2d733b96eba63fcae3c3e7f91acfb95844
Ruan Montelo <ruanmontelo@gmail.com>
2023-01-25 18:35:07 -0300

    Added solution to problem "Is Strictly Palindromic"

---
+++ Medium/IsStrictlyPalindromic.cs
@@ -0,0 +1,25 @@
+﻿namespace Medium;
+
+public class IsStrictlyPalindromic
+{
+    public bool Solve(int n)
+    {
+        try
+        {
+            for (int i = 2; i <= (n - 2); i++)
+            {
+                string s = Convert.ToString(n, n);
+                if (string.Join("", s.Reverse()) != s)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+    }
+}
